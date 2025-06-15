24ed2392bded235429e5b08fd01058dae994dc91
T-Mao <m737579372@gmail.com>
2025-06-15 15:46:27 -0700

    Get

--- src/main/java/com/progressTracker/progress_tracker/repository/ProgressRepository.java
+++ src/main/java/com/progressTracker/progress_tracker/repository/ProgressRepository.java
@@ -1,8 +1,9 @@
 package com.progressTracker.progress_tracker.repository;
 
+import java.util.Optional;                  
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.progressTracker.progress_tracker.model.Progress;
 
 public interface ProgressRepository extends JpaRepository<Progress, Long> {
+    Optional<Progress> findByUserId(Long userId);
 }
