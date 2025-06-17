48dd49ce71c4630a14c1b5ecc4586d3d6f11d38d
Leo Zhang <57935115+Leo10250@users.noreply.github.com>
2025-06-16 22:32:32 -0500

    Modified progress table

--- src/main/java/com/progressTracker/progress_tracker/model/Progress.java
+++ src/main/java/com/progressTracker/progress_tracker/model/Progress.java
@@ -5,20 +5,27 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "progress")
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Progress {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Long userId;
-    private Integer work;
-    private Integer study;
-    private Integer tv;
+    private Integer workHours;
+    private Integer studyHours;
+    private Integer tvHours;
+    private Integer cookingHours;
 }
